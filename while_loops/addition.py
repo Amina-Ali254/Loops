6846744fe1c814b79d9e23a1491d2460a7698876
Amina-Ali254 <amina.ali254@gmail.com>
2024-01-31 10:38:34 +0300

    Addition using while loop

---
+++ while_loops/addition.py
@@ -0,0 +1,8 @@
+#  Write a Python program using while loop that calculates the sum of number form 1 to 500?
+
+a = 1
+total = 0
+while a <= 10: 
+    total = total + a
+    print (total)
+    a = a +1
