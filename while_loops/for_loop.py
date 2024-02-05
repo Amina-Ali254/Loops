6e7c6929156c10765267f33f1e74b2ea61ea7bd1
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-05 09:13:18 +0300

    Creating list using For Loop

---
+++ while_loops/for_loop.py
@@ -0,0 +1,6 @@
+# A for loop is a programming construct that allows you to repeat
+#  write a python program using for loop that iterates over a list of numbers and prints out each number?
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in list:
+    print(number)
