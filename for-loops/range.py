54bd058a7085fca32fb6571de317259fb2f97ecb
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-05 09:28:56 +0300

    Range in for loops

---
+++ for-loops/range.py
@@ -0,0 +1,7 @@
+# Write a Python programm using For Loop to print number 1 to 10 using a range function?
+
+# for number in range (1, 99):
+#     print(number)
+
+for number in range (1,100, 2):
+    print (number)
