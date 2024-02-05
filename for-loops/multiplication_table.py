0e985d9f64ff36523d5121662a9d0d1f69535373
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-05 09:37:42 +0300

    Multiplication table in For Loops

---
+++ for-loops/multiplication_table.py
@@ -0,0 +1,7 @@
+#  Write a Python Programm using the For Loop to print the multiplication table of 5?
+
+# for numb in range (1, 11):
+#     print (f"5 x {numb} = {5 * numb}")
+
+for num in range (1,22):
+    print (f" 7 x {num} = {7 * num}")
