bcc91f5a986a5b64c42139033dc7074d55fa720b
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-05 09:47:49 +0300

    String in for Loops

---
+++ for-loops/string.py
@@ -0,0 +1,10 @@
+# Write a Python Program using For Loop that iterates over a sting and prints out each letter?
+
+# name = 'Cecilia'
+# for letter in name:
+#     print (letter) 
+
+country = 'Kenya'
+for letter in country:
+    print (letter)
+    
