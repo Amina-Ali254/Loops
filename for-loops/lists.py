49e8c38568f32d2ace416640ee260ee321db57d7
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-05 10:02:39 +0300

    Finding greater than using for loop in Python

---
+++ for-loops/lists.py
@@ -0,0 +1,18 @@
+# write a Python program using the For Loop that iterates over a list and checks whether each number is 
+# greater than 3?
+
+# list = (1, 2, 7, 9)
+# for number in list:
+#     if number >= 3:
+#         print (number, 'greater than 3')
+#     else:
+#         print (number, 'not greater than 3')
+
+
+list = (1, 2, 3, 4, 5, 6, 7, 8)
+for number in list:
+    if number <= 5:
+        print (number, 'less than 5')
+    else:
+        print (number, 'greater than 5')
+    
