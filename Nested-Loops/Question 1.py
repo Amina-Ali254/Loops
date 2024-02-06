0b41829ad1830862aa9b382451a57c4e88a7c1f8
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-06 09:38:35 +0300

    Break and Continue using loops in Python

---
+++ Nested-Loops/Question 1.py
@@ -0,0 +1,1 @@
+#  Research more about nested loops and come up with 3 more programs?
---
+++ break & continue/break-and-continue.py
@@ -0,0 +1,6 @@
+# Write a Python programm print numbers from 1 to 10 but breaks loop when in encounters the number 5?
+
+for a in range (1, 11):
+    if a ==5:
+        continue
+    print (a)
