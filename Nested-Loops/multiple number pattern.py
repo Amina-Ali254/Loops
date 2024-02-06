c3c5479ce63aeb349b1e8308d64c4f7b769d3b48
Amina-Ali254 <amina.ali254@gmail.com>
2024-02-06 09:23:42 +0300

    Number patterns using Nested Loops

---
+++ Nested-Loops/multiple number pattern.py
@@ -0,0 +1,6 @@
+# Create Python program using nested loops prints a pattern where each row has the same numbers repeated?
+
+for i in range (1,10):
+    for j in range (i):
+        print (i, end='')
+    print ()
